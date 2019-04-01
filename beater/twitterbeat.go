cef9dc0f2f9d1ca7e409cd75fb5c77389bf042b9
Marcellus Easley <marcellus@easleyclub.com>
2019-04-01 00:26:36 -0400

    add needed packages; TwitterTrend structure; WIP - getTrendingTweet function

--- beater/twitterbeat.go
+++ beater/twitterbeat.go
@@ -3,6 +3,11 @@ package beater
 import (
 	"fmt"
 	"time"
+	"net/http"
+	"encoding/json"
+	"log"
+	"os"
+
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
@@ -17,6 +22,24 @@ type Twitterbeat struct {
 	config config.Config
 	client beat.Client
 }
+
+// TwitterTrend structure contains json structure returned by the Twitter trend API response.
+type TwitterTrend []struct {
+	Trends []struct {
+		Name            string      `json:"name"`
+		URL             string      `json:"url"`
+		PromotedContent interface{} `json:"promoted_content"`
+		Query           string      `json:"query"`
+		TweetVolume     int         `json:"tweet_volume"`
+	} `json:"trends"`
+	AsOf      time.Time `json:"as_of"`
+	CreatedAt time.Time `json:"created_at"`
+	Locations []struct {
+		Name  string `json:"name"`
+		Woeid int    `json:"woeid"`
+	} `json:"locations"`
+}
+
 
 // New creates an instance of twitterbeat.
 func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
@@ -68,4 +91,19 @@ func (bt *Twitterbeat) Run(b *beat.Beat) error {
 func (bt *Twitterbeat) Stop() {
 	bt.client.Close()
 	close(bt.done)
+}
+
+
+//
+func getTrendingTweet () {
+	trendsPlaceURL := "https://api.twitter.com/1.1/trends/place.json?id=23424977"
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", trendsPlaceURL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Authorization", os.Getenv(BEARER_TOKEN))
+	resp, err := client.Do(req)
 }
